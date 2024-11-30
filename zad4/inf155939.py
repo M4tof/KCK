2199c646f2b2531811623a0f499d4c10d0f355dd
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2024-11-30 23:02:04 +0100

    Excited pointing sojak at 88%

--- zad4/inf155939.py
+++ zad4/inf155939.py
@@ -2,8 +2,25 @@ import numpy as np
 import scipy.io.wavfile
 import sys
 
-maleRange = [85,180]
-femaleRange = [165,255]
+unsureCutOff = 170
+maleRange = [85, 180]
+femaleRange = [165, 255]
+
+def normalize(signal):
+    return signal / np.max(np.abs(signal))
+
+def apply_window(signal):
+    beta = 14
+    window = np.kaiser(len(signal),beta) #0
+    return signal * window
+
+def preprocess(signal, sample_rate):
+    # If stereo, convert to mono
+    if len(signal.shape) > 1:
+        signal = np.mean(signal, axis=1)
+    signal = normalize(signal)
+    signal = apply_window(signal)
+    return signal
 
 def cepstral(signal):
     spectrum = np.fft.fft(signal)
@@ -11,7 +28,7 @@ def cepstral(signal):
     cepstrum = np.fft.fft(log_spectrum).real
     return cepstrum
 
-def findFrequency(cepstrum,sampleRate):
+def findFrequency(cepstrum, sampleRate):
     minimum = maleRange[0]
     maximum = femaleRange[1]
     min_quefrency = int(sampleRate / maximum)
@@ -23,36 +40,27 @@ def findFrequency(cepstrum,sampleRate):
     found_frequency = sampleRate / peak
     return found_frequency
 
-def ifWoman(rawData,sampleRate):
-    Male = False
-    Female = False
+def ifWoman(rawData, sampleRate):
 
     cepstrumData = cepstral(rawData)
+    fr = findFrequency(cepstrumData, sampleRate)
 
-    fr = findFrequency(cepstrumData,sampleRate)
+    if fr <= unsureCutOff:
+        return 'M'
 
-    if maleRange[0] <= fr <= maleRange[1]:
-        Male = True
-    elif femaleRange[0] <= fr <= femaleRange[1]:
-        Female = True
-    
-    if Female:
-        return 'K'
-    elif Male:
-        return 'M'
-    else:
-        return 'M'
+    return 'K'
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit(1)
 
     filename = sys.argv[1]
-    
+
     verdict = 'K'
 
     try:
         rate, signal = scipy.io.wavfile.read(filename)
-        verdict = ifWoman(signal,rate)
+        signal = preprocess(signal, rate)
+        verdict = ifWoman(signal, rate)
     finally:
         print(verdict)
