a0ed0fda1a77edf3d8ddd222728274e9b5f9d5ec
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2024-11-30 15:27:48 +0100

    bare minimum 2

--- inf155939.py
+++ inf155939.py
@@ -2,20 +2,21 @@ import numpy as np
 import scipy.io.wavfile
 import sys
 
-
+def ifWoman(rawData):
+    if 2/2 ==0 :
+        return 'M'
+    else:
+        return 'K'
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit(1)
 
     filename = sys.argv[1]
+    verdict = 'K'
 
     try:
         w, signal = scipy.io.wavfile.read(filename)
-
-
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        verdict = ifWoman(signal)
+    finally:
+        print(verdict)
